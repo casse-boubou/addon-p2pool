e888b037f9fed60e2d66e38a5d8cc5835df8bb4f
Frosch <erdnaxela02@hotmail.fr>
2022-11-06 17:15:00 +0100

    Delete 18080 port

--- p2pool/rootfs/etc/cont-init.d/30-config.sh
+++ p2pool/rootfs/etc/cont-init.d/30-config.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 # ==============================================================================
-# Home Assistant Add-on: MoneroDxP2Pool
-# This validates config before launch monerod or p2pool exec
+# Home Assistant Add-on: P2Pool
+# This validates config before launch p2pool exec
 # ==============================================================================
 
 
